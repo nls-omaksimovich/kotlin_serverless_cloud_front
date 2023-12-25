e26f22e2963680662c62f2239b0029d9abfa3e5c
Oleg Maksimovich <omaksimovich@nautilus.com>
2023-12-25 14:22:32 +0100

    introduce error handling and text param

--- src/main/kotlin/com/example/Handler.kt
+++ src/main/kotlin/com/example/Handler.kt
@@ -4,8 +4,12 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.example.constant.CONTENT_TYPE_HEADER_KEY
+import com.example.constant.JSON_HEADER_VALUE
+import com.example.constant.MP3_HEADER_VALUE
+import com.example.error.ErrorResponse
 import com.google.gson.GsonBuilder
-import java.util.*
+import java.util.Base64
 
 class Handler : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private val mapper = GsonBuilder().setPrettyPrinting().create()
@@ -14,27 +18,59 @@ class Handler : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyRespo
     override fun handleRequest(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
         val logger = context.logger
 
+        logRequestParams(input, context)
+
+        val textParam = input.extractTextParam()
+        logger.log("text param value=[$textParam]")
+
+        if (textParam == null) {
+            val errorResponse = ErrorResponse(400, "text param is null")
+
+            return APIGatewayProxyResponseEvent()
+                .withStatusCode(400)
+                .withHeaders(
+                    mapOf(
+                        CONTENT_TYPE_HEADER_KEY to JSON_HEADER_VALUE
+                    )
+                )
+                .withBody(mapper.toJson(errorResponse))
+        } else {
+            val mp3FileData = getMp3FileData()
+            val base64EncodedMp3 = Base64.getEncoder().encodeToString(mp3FileData)
+
+            return APIGatewayProxyResponseEvent()
+                .withStatusCode(200)
+                .withHeaders(
+                    mapOf(
+                        CONTENT_TYPE_HEADER_KEY to MP3_HEADER_VALUE
+                    )
+                )
+                .withIsBase64Encoded(true)
+                .withBody(base64EncodedMp3)
+        }
+    }
+
+    private fun logRequestParams(input: APIGatewayProxyRequestEvent, context: Context) {
+        val logger = context.logger
+
         // log execution details
         logger.log("ENVIRONMENT VARIABLES: " + mapper.toJson(System.getenv()))
         logger.log("CONTEXT: " + mapper.toJson(context))
         // process event
         logger.log("EVENT: " + mapper.toJson(input))
-
-        val mp3FileData = getMp3FileData()
-        val base64EncodedMp3 = Base64.getEncoder().encodeToString(mp3FileData)
-
-        return APIGatewayProxyResponseEvent()
-            .withStatusCode(200)
-            .withHeaders(
-                mapOf(
-                    "Content-Type" to "audio/mpeg",
-                    "Content-Encoding" to "base64"
-                )
-            )
-            .withIsBase64Encoded(true)
-            .withBody(base64EncodedMp3)
     }
 
     private fun getMp3FileData(): ByteArray =
         this::class.java.getResource(mp3FilePath)?.readBytes() ?: ByteArray(0)
+
+    /**
+     * if no queryStringParameters has passed, APIGatewayProxyRequestEvent.queryStringParameters is null
+     */
+    private fun APIGatewayProxyRequestEvent.extractTextParam(): String? {
+        val queryStringParameters = this.queryStringParameters
+        return if (queryStringParameters != null) {
+            this.queryStringParameters["text"]
+        } else
+            null
+    }
 }
---
+++ src/main/kotlin/com/example/constant/HttpConstants.kt
@@ -0,0 +1,5 @@
+package com.example.constant
+
+const val CONTENT_TYPE_HEADER_KEY = "Content-Type"
+const val MP3_HEADER_VALUE = "audio/mp3"
+const val JSON_HEADER_VALUE = "application/json"
